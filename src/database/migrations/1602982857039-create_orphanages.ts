247d6148cc68cd6815887424f60b0b7d16c049ed
Celio Maymone <devdesignerstudio@protonmail.com>
2020-10-26 04:21:34 -0300

    Deploy do app Happy criado na NLW#3 usando Heroku

---
+++ src/database/migrations/1602982857039-create_orphanages.ts
@@ -0,0 +1,60 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createOrphanages1602982857039 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      // REALIZAR AS ALTERAÇÕES NO DATABASE
+      await queryRunner.createTable(new Table({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'latitude',
+            type: 'varchar',
+          },
+          {
+            name: 'longitude',
+            type: 'varchar',
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            default: false
+          },
+          // {
+          //   name: 'contact',
+          //   type: 'text',
+          // },
+        ],
+      }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      // DESFAZER AS ALTERAÇÕES NO DATABASE FEITO PELO MÉTODO UP
+      await queryRunner.dropTable('orphanages');
+  }
+
+}
